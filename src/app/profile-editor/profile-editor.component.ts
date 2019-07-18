0e7f63b34b41182aaf886fde2b62e5bd666df8fa
sravyareddy27 <yashwanthreddy123>
2019-07-18 15:06:16 +0530

    reactiveforms

---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent {
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
+  });
+  constructor(private fb: FormBuilder) { }
+get aliases() {
+  return this.profileForm.get('aliases') as FormArray;
+}
+addAlias() {
+  this.aliases.push(this.fb.control(''));
+}
+}
